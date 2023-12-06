5a55069c835294507293fba25f295a247c4fe20a
Math3usH <matheushenriquecalsavara@gmail.com>
2023-12-06 09:40:49 -0300

    .

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
+// Função para adicionar valor ao display
 function addToDisplay(value) {
+    //let contento = document.getElementById('display').value.toString()+value.toString();
     document.getElementById('display').value += value;
+     //document.getElementById('display').value = contento;
+
 }
 
-function calculate() {
-    try {
-        document.getElementById('display').value = eval(document.getElementById('display').value);
-    } catch (error) {
-        document.getElementById('display').value = 'Erro';
-    }
+// Função para calcular o resultado
+function calculate  () {
+    let display = document.getElementById('display');
+    let resultado = eval(display.value); // Usando eval para calcular a expressão inserida
+    document.getElementById('display1').value = display.value + "="+resultado;
+    display.value = resultado;
+
 }
 
+// Função para limpar o display
 function clearDisplay() {
     document.getElementById('display').value = '';
 }
