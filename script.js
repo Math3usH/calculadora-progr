babe633ab63245a8be7d82a659a0a7ad9960984a
Math3usH <matheushenriquecalsavara@gmail.com>
2023-12-07 09:26:33 -0300

    .

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 // Função para adicionar valor ao display
-function adicionarAoDisplay(value) {;
+function adicionarAoDisplay(value) {
+    ;
     //Função serve para adicionar valores ao campo de exibição da calculadora sempre que um botão numérico ou operador for pressionado
-     document.getElementById('display').value += value; 
+    document.getElementById('display').value += value;
 }
 
 // Função para calcular o resultado
@@ -18,4 +19,31 @@ function calcular() {
 // Função para limpar o display
 function limparDisplay() {
     document.getElementById('display').value = '';
+}
+// função para limpar os dois displays
+function limparDisplays() {
+    document.getElementById('display1').value = '';
+    document.getElementById('display').value = '';
+}
+
+// Adicionando evento para aceitar entrada do teclado
+document.addEventListener('keydown', function (event) {
+    const tecla = event.key;
+
+    if (!isNaN(parseInt(tecla)) || tecla === '+' || tecla === '-' || tecla === '*' || tecla === '/' || tecla === '.' || tecla === 'Enter') {
+        event.preventDefault(); // Evita que a tecla pressionada cause ação padrão
+
+        if (tecla === 'Enter') {
+            calcular(); // Se 'Enter' for pressionado, calcula o resultado
+        } else {
+            adicionarAoDisplay(tecla); // Adiciona o valor do teclado ao display
+        }
+    }
+    
+});
+// função para apagar o ultimo caractere
+function apagarUltimoCaractere() {
+    let display = document.getElementById('display');
+    display.value = display.value.slice(0, -1);
+
 }
